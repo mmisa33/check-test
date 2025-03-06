63e9e48396d0ec0ed143ab752153d9f18afa3588
misa <mmichiura33@gmail.com>
2025-03-06 21:54:26 +0900

    ContactControllerの作成とトップページへの仮ルーティング追加

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    // お問い合わせフォーム入力ページ表示
+    public function index()
+    {
+        return view('index');
+    }
+}

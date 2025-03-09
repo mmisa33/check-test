cefc4d25aa908a0623676ae7d08f17d48764aaba
misa <mmichiura33@gmail.com>
2025-03-09 09:40:10 +0900

    サンクスページを作成

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,9 +1,36 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('css')
-<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FashionablyLate</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika&display=swap" rel="stylesheet">
+</head>
 
-@section('content')
+<body>
+    <!-- 背景 -->
+    <div class="thanks__background">
+        <span class="background-text">Thank you</span>
+    </div>
 
-@endsection
+    <!-- サンクスコンテンツ -->
+    <div class="thanks__content">
+        <!-- メッセージ -->
+        <div class="thanks__heading">
+            <h2>お問い合わせありがとうございました</h2>
+        </div>
+
+        <!-- HOMEボタン -->
+        <div class="home-button">
+            <a href="/" class="home-button__submit">HOME</a>
+        </div>
+    </div>
+</body>
+
+</html>
